5ad7187b237383586be1b89a728398a808bfdcd2
Tomasz Janas <tomek@janas.dev>
2023-05-22 18:24:33 +0200

    disable preboot dhcp

--- hosts/lenovo/hardware-configuration.nix
+++ hosts/lenovo/hardware-configuration.nix
@@ -22,7 +22,7 @@
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  networking.interfaces.enp3s0.useDHCP = lib.mkDefault true;
+  # networking.interfaces.enp3s0.useDHCP = lib.mkDefault true;
   # networking.interfaces.virbr0.useDHCP = lib.mkDefault true;
   # networking.interfaces.wlp2s0.useDHCP = lib.mkDefault true;
 
