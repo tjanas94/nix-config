6c4b35cc370bcd86f5e02997dde464e35124ddf0
Tomasz Janas <tomek@janas.dev>
2023-09-03 07:26:28 +0200

    Add qwerty bindings in harpoon

--- config/nvim/lua/core/map.lua
+++ config/nvim/lua/core/map.lua
@@ -19,7 +19,6 @@ vim.keymap.set('n', '<leader>Y', [['+Y]])
 vim.keymap.set({ 'n', 'x' }, '<leader>d', [['_d]])
 
 vim.keymap.set('n', 'Q', '<nop>')
-vim.keymap.set('n', '<leader>f', vim.lsp.buf.format)
 
 vim.keymap.set('n', '<C-k>', '<cmd>cnext<CR>zz')
 vim.keymap.set('n', '<C-j>', '<cmd>cprev<CR>zz')
