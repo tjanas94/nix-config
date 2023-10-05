941b45f1feb3c2f44b069b8f0a8177126c174778
Tomasz Janas <tomek@janas.dev>
2023-10-05 17:49:38 +0200

    Fix yank to end of line mapping

--- config/nvim/lua/core/map.lua
+++ config/nvim/lua/core/map.lua
@@ -14,7 +14,7 @@ vim.keymap.set('x', '<leader>p', [["_dP]])
 
 -- next greatest remap ever : asbjornHaland
 vim.keymap.set({ 'n', 'x' }, '<leader>y', [["+y]])
-vim.keymap.set('n', '<leader>Y', [["+Y]])
+vim.keymap.set('n', '<leader>Y', [["+y$]])
 
 vim.keymap.set({ 'n', 'x' }, '<leader>d', [["_d]])
 
