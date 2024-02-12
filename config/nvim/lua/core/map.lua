85c9daaff57cdb29b22e9b661c0039ae33da8b43
Tomasz Janas <tomek@janas.dev>
2024-02-12 18:54:26 +0100

    Add arrow bindings

--- config/nvim/lua/core/map.lua
+++ config/nvim/lua/core/map.lua
@@ -3,6 +3,8 @@ vim.g.maplocalleader = '\\'
 
 vim.keymap.set('x', 'J', ':m \'>+1<CR>gv=gv')
 vim.keymap.set('x', 'K', ':m \'<-2<CR>gv=gv')
+vim.keymap.set('x', '<S-Down>', ':m \'>+1<CR>gv=gv')
+vim.keymap.set('x', '<S-Up>', ':m \'<-2<CR>gv=gv')
 
 vim.keymap.set('n', 'J', 'mzJ`z')
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
@@ -25,6 +27,10 @@ vim.keymap.set('n', '<C-k>', '<cmd>cnext<CR>zz')
 vim.keymap.set('n', '<C-j>', '<cmd>cprev<CR>zz')
 vim.keymap.set('n', '<leader>k', '<cmd>lnext<CR>zz')
 vim.keymap.set('n', '<leader>j', '<cmd>lprev<CR>zz')
+vim.keymap.set('n', '<C-Up>', '<cmd>cnext<CR>zz')
+vim.keymap.set('n', '<C-Down>', '<cmd>cprev<CR>zz')
+vim.keymap.set('n', '<leader><Up>', '<cmd>lnext<CR>zz')
+vim.keymap.set('n', '<leader><Down>', '<cmd>lprev<CR>zz')
 
 vim.keymap.set('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.keymap.set('n', '<leader>x', '<cmd>!chmod +x %<CR>', { silent = true })
