808521dc20a8c0e05aef6f4080268b74c3268b59
Tomasz Janas <tomek@janas.dev>
2024-02-16 22:47:08 +0100

    Switch projects without tmux prefix

--- home/tjanas/common/cli/tmux.nix
+++ home/tjanas/common/cli/tmux.nix
@@ -18,15 +18,15 @@
       set -g set-titles-string '#S / #W'
 
       bind r source-file ~/.config/tmux/tmux.conf
-      bind C-s switch-client -l
       bind f neww switch-project
       bind C-f run-shell 'clone-tmux-client'
-      bind M-h neww 'vi ~/workspace/projects.txt'
-      bind M-a run-shell 'pwd >> ~/workspace/projects.txt'
-      bind M-n run-shell 'switch-project "$(sed -n "1{p;q}" ~/workspace/projects.txt)"'
-      bind M-e run-shell 'switch-project "$(sed -n "2{p;q}" ~/workspace/projects.txt)"'
-      bind M-i run-shell 'switch-project "$(sed -n "3{p;q}" ~/workspace/projects.txt)"'
-      bind M-o run-shell 'switch-project "$(sed -n "4{p;q}" ~/workspace/projects.txt)"'
+      bind -n C-M-s switch-client -l
+      bind -n C-M-h neww 'vi ~/workspace/projects.txt'
+      bind -n C-M-a run-shell 'pwd >> ~/workspace/projects.txt'
+      bind -n C-M-n run-shell 'switch-project "$(sed -n "1{p;q}" ~/workspace/projects.txt)"'
+      bind -n C-M-e run-shell 'switch-project "$(sed -n "2{p;q}" ~/workspace/projects.txt)"'
+      bind -n C-M-i run-shell 'switch-project "$(sed -n "3{p;q}" ~/workspace/projects.txt)"'
+      bind -n C-M-o run-shell 'switch-project "$(sed -n "4{p;q}" ~/workspace/projects.txt)"'
     '';
     historyLimit = 50000;
     keyMode = "vi";
