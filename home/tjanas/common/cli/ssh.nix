95118caa492e2a77fabb13fef367337e2be89b6a
Tomasz Janas <tomek@janas.dev>
2024-05-03 13:45:44 +0200

    Reorganize ssh config

--- home/tjanas/common/cli/ssh.nix
+++ home/tjanas/common/cli/ssh.nix
@@ -5,19 +5,8 @@
     addKeysToAgent = "yes";
     controlMaster = "auto";
     controlPersist = "60s";
+    controlPath = "~/.ssh/sockets.d/%r@%n:%p";
     includes = [ "config.d/*" ];
-    extraConfig = ''
-      IdentitiesOnly yes
-      IdentityFile ~/.ssh/id_ed25519_5.pub
-      IdentityFile ~/.ssh/id_ed25519_4.pub
-      IdentityFile ~/.ssh/id_rsa_3.pub
-      IdentityFile ~/.ssh/id_ed25519_2.pub
-      IdentityFile ~/.ssh/id_ed25519_1.pub
-    '';
-    matchBlocks."github github.com" = {
-      hostname = "github.com";
-      user = "git";
-    };
   };
 
   home.file.".ssh" = {
--- hosts/common/users/tjanas/authorized-keys.nix
+++ hosts/common/users/tjanas/authorized-keys.nix
@@ -1,7 +1,7 @@
 [
-  (builtins.readFile ../../../../config/ssh/id_ed25519_1.pub)
-  (builtins.readFile ../../../../config/ssh/id_ed25519_2.pub)
-  (builtins.readFile ../../../../config/ssh/id_rsa_3.pub)
-  (builtins.readFile ../../../../config/ssh/id_ed25519_4.pub)
-  (builtins.readFile ../../../../config/ssh/id_ed25519_5.pub)
+  (builtins.readFile ../../../../config/ssh/public.d/id_ed25519_1.pub)
+  (builtins.readFile ../../../../config/ssh/public.d/id_ed25519_2.pub)
+  (builtins.readFile ../../../../config/ssh/public.d/id_rsa_3.pub)
+  (builtins.readFile ../../../../config/ssh/public.d/id_ed25519_4.pub)
+  (builtins.readFile ../../../../config/ssh/public.d/id_ed25519_5.pub)
 ]
