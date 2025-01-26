e0d0132ae7dd63630ddd26a2e6109124c9faf908
Tomasz Janas <tomek@janas.dev>
2025-01-26 11:54:17 +0100

    Update autorandr setup

--- home/tjanas/common/programs/autorandr.nix
+++ home/tjanas/common/programs/autorandr.nix
@@ -1,8 +1,6 @@
+{ config, ... }:
 {
   programs.autorandr.enable = true;
 
-  xdg.configFile.autorandr = {
-    source = ../../../../config/autorandr;
-    recursive = true;
-  };
+  xdg.configFile.autorandr.source = config.lib.file.mkOutOfStoreSymlink "/home/tjanas/workspace/github.com/tjanas94/nix-config/master/config/autorandr";
 }
